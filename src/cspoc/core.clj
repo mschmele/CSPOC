9571c0aada054dac2767c85d7e186d76cc067dee
Mark Schmele <mark.schmele@tegna.com>
2016-08-25 14:31:27 -0400

    basic withdraw, deposit, transfer, and associated helper functions implemented

--- src/cspoc/core.clj
+++ src/cspoc/core.clj
@@ -1,7 +1,36 @@
 (ns cspoc.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn select-values (comp vals select-keys))
+
+(defn get-account-from-id
+  [id]
+  (first (filter #(= (% :id) id) accounts)))
+
+(defn account-balance
+  [id]
+  (first (select-values (get-account-from-id accounts id) [:balance])))
+
+(defn deposit [amount account]
+  (update (get-account-from-id account) :balance + amount))
+
+(defn withdraw
+  "Basic withdraw function. Does NOT handle validation."
+  [amount account]
+  (update (get-account-from-id account) :balance - amount))
+
+(defn transfer
+  "Transfer money from one account to another.
+   Returns the updated accounts collection."
+  [from-id to-id amount]
+  (if (< (account-balance accounts from-id) amount)
+      (println "This will cause an overdraft!")
+      (do
+       (withdraw amount from-id)
+       (deposit amount to-id))))
+
+
+(def accounts
+  [{:id "jason" :balance 100}
+   {:id "chris" :balance 0}
+   {:id "mark" :balance 50000}])
