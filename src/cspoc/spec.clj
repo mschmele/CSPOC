5f8977376af33e68bc68ed68e10e85653221f074
Mark Schmele <mschmele@mbp-mschmele.edn.ecs.nasa.gov>
2016-08-30 14:21:50 -0400

    Define types, fix function specs

--- src/cspoc/spec.clj
+++ src/cspoc/spec.clj
@@ -2,29 +2,41 @@
   (:require [clojure.spec :as s])
   (:require [cspoc.core :as cspoc]))
 
+(s/check-asserts true)
+
+;; Here is an account. It is a map that requires an id and balance.
+;; The type field is optional according to this spec.
+(s/def ::id #(string? %))
+(s/def ::balance #(number? %))
+(s/def ::type #(string? %))
+(s/def ::account (s/keys :req [::id ::balance]
+                         :opt [::type]))
+
+(s/def ::accounts (s/coll-of ::account))
+
 (s/fdef cposc/get-account-from-id
-        :args [(s/assert vector? ::accounts)
-               (s/assert string? ::id)]
-        :ret map?)
+        :args [#(s/valid? ::accounts %)
+               #(s/valid? ::id %)]
+        :ret #(s/valid? ::account %))
 
 (s/fdef cposc/account-balance
-        :args [(s/assert vector? ::accounts)
-               (s/assert string? ::id)]
-        :ret number?)
+        :args [#(s/valid? ::accounts %)
+               #(s/valid? ::id %)]
+        :ret #(number? %))
 
 (s/fdef cposc/deposit
-        :args [(s/assert number? ::amount)
-               (s/assert map? :account)]
-        :ret map?)
+        :args [#(s/assert ::amount %)
+               #(s/assert ::account %)]
+        :ret #(s/valid? ::account %))
 
 (s/fdef cposc/withdraw
-        :args [(s/assert number? ::amount)
-               (s/assert map? ::account)]
-        :ret map?)
+        :args [#(s/assert ::amount %)
+               #(s/assert ::account %)]
+        :ret #(s/valid? ::account %))
 
 (s/fdef cposc/transfer
-        :args [(s/assert vector? ::accounts)
-               (s/assert string? ::from-id)
-               (s/assert string? ::to-id)
-               (s/assert number? ::amount)]
-        :ret vector?)
+        :args [#(s/assert ::accounts %)
+               #(s/assert ::from-id %)
+               #(s/assert ::to-id %)
+               #(s/assert ::amount %)]
+        :ret #(s/valid? ::accounts %))
