536fb4a01abbd56b7eb9022fe6c8fa298fb6aaf5
Mark Schmele <mschmele@mbp-mschmele.edn.ecs.nasa.gov>
2016-08-30 09:16:44 -0400

    Move spec into another file because that seems like the right thing to do

--- src/cspoc/core.clj
+++ src/cspoc/core.clj
@@ -5,32 +5,24 @@
 (def select-values (comp vals select-keys))
 
 (defn get-account-from-id [accounts id]
-  (s/assert string? id)
-  (s/assert vector? accounts)
   (first (filter #(= (% :id) id) accounts)))
 
 (defn account-balance [accounts id]
-  (s/assert string? id)
-  (s/assert vector? accounts)
   (first (select-values (get-account-from-id accounts id) [:balance])))
 
 (defn deposit [amount account]
-  (s/assert number? amount)
-  (s/assert map? account)
   (update account :balance + amount))
 
 (defn withdraw [amount account]
-  (s/assert number? amount)
-  (s/assert map? account)
-  (s/assert (> (account :balance) amount))
   (update account :balance - amount))
 
 (defn transfer [accounts from-id to-id amount]
   "Transfer money from one account to another.
    Returns the updated accounts collection."
-      (conj '[]
-         (withdraw amount (get-account-from-id accounts from-id))
-         (deposit amount (get-account-from-id accounts to-id))))
+  (conj '[]
+    (withdraw amount (get-account-from-id accounts from-id))
+    (deposit amount (get-account-from-id accounts to-id))
+    (filter #(= ) accounts)))
 
 ; This is just a bit of test data. Nothing to see here
 (def accounts
---
+++ src/cspoc/spec.clj
@@ -0,0 +1,33 @@
+(ns spec-demo
+  (:require [clojure.spec :as s])
+  (:require [cspoc.core :as cspoc]))
+
+(s/fdef cposc/get-account-from-id
+        :args [(s/assert vector? ::accounts)
+               (s/assert string? ::id)]
+        :ret map?)
+
+(s/fdef cposc/account-balance
+        :args [(s/assert vector? ::accounts)
+               (s/assert string? ::id)]
+        :ret number?)
+
+(s/fdef cposc/deposit
+        :args [(s/assert number? ::amount)
+               (s/assert map? :account)]
+        :ret map?)
+
+(s/fdef cposc/withdraw
+        :args [(s/assert number? ::amount)
+               (s/assert map? ::account)]
+        :ret map?)
+
+(s/fdef cposc/transfter
+        :args [(s/assert vector? ::accounts)
+               (s/assert string? ::from-id)
+               (s/assert string? ::to-id)
+               (s/assert number? ::amount)]
+        :ret vector?)
+
+
+
